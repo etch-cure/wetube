a53e39b7de3346a5b4c8ea9e35489dd37b1498ca
minha <fbalsgk94@naver.com>
2020-12-31 01:52:04 +0900

    [초기][메인 스크립트] app.js - 각종 미들웨어 및 라우터 등록 db.js - 가짜 데이터 init.js - 엔트리 포인트 middlewares.js - locals 데이터 미들웨어 routes.js - 라우터 정의

---
+++ db.js
@@ -0,0 +1,46 @@
+export const videos = [
+    {
+        id:514651,
+        title: 'Video awesome',
+        description: 'This is Sample',
+        views: 24,
+        videoFile: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+        creator: {
+            name: 'minha',
+            email: 'etchcure@gmail.com'
+        }
+    },
+    {
+        id:123456,
+        title: 'Video nice',
+        description: 'This is Sample',
+        views: 24,
+        videoFile: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+        creator: {
+            name: 'minha',
+            email: 'etchcure@gmail.com'
+        }
+    },
+    {
+        id:111111,
+        title: 'Video cool',
+        description: 'This is Sample',
+        views: 24,
+        videoFile: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+        creator: {
+            name: 'minha',
+            email: 'etchcure@gmail.com'
+        }
+    },
+    {
+        id:222222,
+        title: 'Video beautiful',
+        description: 'This is Sample',
+        views: 24,
+        videoFile: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+        creator: {
+            name: 'minha',
+            email: 'etchcure@gmail.com'
+        }
+    }
+]
---
+++ init.js
@@ -0,0 +1,9 @@
+import app from './app'
+
+const PORT = 4000
+
+const handelListening = () => {
+    console.log(`👌 Listening on: http://localhost:${PORT}`)
+}
+
+app.listen(PORT, handelListening)
